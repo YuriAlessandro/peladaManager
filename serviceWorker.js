5fa50d96fc1b2f253b93e650216edc2f6531a55f
Yuri Martins <yuri.alessandro@hotmail.com>
2024-10-23 10:59:21 -0300

    change sw version

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = `volley_score_cache_v4`;
+const CACHE_NAME = `volley_score_cache_v5`;
 
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {

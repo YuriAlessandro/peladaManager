1d96277c6265e84654d9eafe7b8c02240b611708
Isaac Batista <isaacbatst@gmail.com>
2024-12-11 11:46:39 -0300

    fix: delete old caches

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,3 +1,13 @@
+const CACHE_NAME = `volley_score_cache_v6`;
+
 self.addEventListener('install', (event) => {
   console.log('Service Worker instalado');
+  event.waitUntil((async () => {
+      const deleted = caches.delete(CACHE_NAME);
+      if(!deleted) {
+        alert('Cache não deletado');
+      } else {
+        alert('Cache deletado');
+      }
+    })());
 });

a47d787079e193937e7f3e6066a2739e3d97f962
Yuri Martins <yuri.alessandro@hotmail.com>
2025-07-30 17:07:30 -0300

    use hash router to test

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./app.tsx";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/peladaManager">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
